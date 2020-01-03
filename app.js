96f01a0476c8802287f6ce347005344c5b92ecb3
gemanepa <contacto@gemanepa.com>
2020-01-03 20:19:43 -0300

    Chalk ready

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const cookieParser = require ('cookie-parser');
 const indexRouter = require ('./routes/index');
 const usersRouter = require ('./routes/users');
 const httpLogger = require('morgan')
-const log = require('chalk')
+const log = require('./utils/consoleMessage')
 
 const app = express();
 
-log('info', 'Running application')
+log('viinfo', 'RUNNING APPLICATION ON PORT 3000')
+
 app.use(httpLogger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+log('header', 'PATHS', true)
+log('info', '/')
+log('info', '/users')
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
