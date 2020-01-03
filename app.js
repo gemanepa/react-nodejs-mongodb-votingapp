0bb8933e794735a4ace1b14964d54720c6248b4f
gemanepa <contacto@gemanepa.com>
2020-01-03 20:08:52 -0300

    helpers/consoleMessage.js

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const path = require ('path');
 const cookieParser = require ('cookie-parser');
 const indexRouter = require ('./routes/index');
 const usersRouter = require ('./routes/users');
-const logger = require('morgan')
+const httpLogger = require('morgan')
+const log = require('chalk')
+
 const app = express();
 
-app.use(logger('dev'));
+log('info', 'Running application')
+app.use(httpLogger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
