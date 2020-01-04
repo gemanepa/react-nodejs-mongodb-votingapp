2b9fabb5ff30b3947544f08d81d057a5b215f8d0
gemanepa <contacto@gemanepa.com>
2020-01-03 22:31:50 -0300

    emotion working

--- views/index.jsx
+++ views/index.jsx
@@ -1,9 +1,24 @@
 import Layout from './shared-ui/Layout'
+import { css, cx } from 'emotion'
+
+const color = 'white'
 
 export default function Index({ message }) {
     return (
      <Layout title="Lalala">
         <p>{message}</p>
+        <button className={buttonCSS}>
+            Hover to change color.
+        </button>
     </Layout>
     )
 }
+
+const buttonCSS = css`
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    &:hover {
+    color: ${color};
+}`
