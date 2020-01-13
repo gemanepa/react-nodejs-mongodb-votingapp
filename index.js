2b0e44b17e37916a6d3ad7a516c8c73bb22511a5
gemanepa <contacto@gemanepa.com>
2020-01-12 22:22:11 -0300

    Added MongoDB connection and on termination closing connection

--- index.js
+++ index.js
@@ -1,37 +1,74 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+
+// MongoDB Modules
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
+
+// View Modules
 const register = require('@react-ssr/express/register');
+
+// App lifecycle handlers
+const onTerminationSignal = require('death');
+
+// Logging Modules
 const httpLogger = require('morgan');
 const log = require('./utils/consoleMessage');
+
+// Endpoints Modules
 const viewsRouter = require('./routes/views');
 const voterRouter = require('./routes/voter');
 const voteRouter = require('./routes/vote');
 
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+
 app.use(httpLogger('dev'));
 app.use(express.static('public'));
 
-(async () => {
+log('info', 'Connecting application with MongoDB...');
+MongoClient.connect('mongodb://localhost:27017/utn-dw-votacion', { useUnifiedTopology: true }, (err, database) => {
+  assert.equal(null, err);
+  log('info', 'Connection established');
+
+
+  (async () => {
   // register `.jsx` or `.tsx` as a view template engine
-  await register(app);
+    await register(app);
 
-  app.use('/', viewsRouter);
-  app.use('/voter', voterRouter);
-  app.use('/vote', voteRouter);
+    app.use('/', viewsRouter);
 
-  app.listen(3000, () => {
-    log('header', 'RUNNING APPLICATION ON PORT 3000');
-    log('info', 'View Logic Handler: React');
-    log('info', 'View Styles Handler: Emotion');
-    log('info', 'View Template Engine: JSX');
-    log('info', 'HTTP Requests Logs: Morgan');
-    log('info', 'App Logs: Chalk');
-    log('header', 'ENDPOINTS', true);
-    log('info', 'GET  | /');
-    log('info', 'GET  | /voter');
-    log('info', 'POST | /vote');
+    app.use('/voter', (req, res, next) => {
+      req.database = database;
+      next();
+    }, voterRouter);
+
+    app.use('/vote', (req, res, next) => {
+      req.database = database;
+      next();
+    }, voteRouter);
+
+    app.listen(3000, () => {
+      log('header', 'RUNNING APPLICATION ON PORT 3000', true);
+      log('info', 'View Logic Handler: React');
+      log('info', 'View Styles Handler: Emotion');
+      log('info', 'View Template Engine: JSX');
+      log('info', 'Database: MongoDB');
+      log('info', 'HTTP Requests Logs: Morgan');
+      log('info', 'App Logs: Chalk');
+      log('header', 'ENDPOINTS', true);
+      log('info', 'GET  | /');
+      log('info', 'GET  | /voter');
+      log('info', 'POST | /vote');
+    });
+  })();
+
+  onTerminationSignal(() => {
+    log('info', 'Closing database connection', true);
+    database.close();
+    log('info', 'Closed');
+    log('info', 'Exiting process...');
+    process.exit();
   });
-})();
+});
