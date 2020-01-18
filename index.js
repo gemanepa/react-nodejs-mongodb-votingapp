b79fdbb1928a14e1d22712124eff6a6729437652
gemanepa <contacto@gemanepa.com>
2020-01-17 23:05:59 -0300

    Added CORS

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const assert = require('assert');
 // View Modules
 const register = require('@react-ssr/express/register');
 
+// Security Modules
+const cors = require('cors');
+
 // App lifecycle handlers
 const onTerminationSignal = require('death');
 
@@ -25,7 +28,7 @@ const setCandidates = require('./db/setCandidates');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors())
 app.use(httpLogger('dev'));
 app.use(express.static('public'));
 
