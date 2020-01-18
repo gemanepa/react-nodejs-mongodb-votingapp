e8ff240a87ded3fabe05814aa21294f7e9a805e6
gemanepa <contacto@gemanepa.com>
2020-01-18 10:16:53 -0300

    Fix lint errors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const setCandidates = require('./db/setCandidates');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 app.use(httpLogger('dev'));
 app.use(express.static('public'));
 
